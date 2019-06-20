956bb6957b9520105df82cbf09da4e6689a5790a
pevirtual2 <minsley@productiveedge.com>
2019-06-20 15:16:38 -0500

    Full Project Push
    First room space test

---
+++ OWO-Storyboard/Assets/changeMat.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+
+
+public class changeMat : MonoBehaviour {
+
+
+
+	public SteamVR_Input_Sources handType;
+	public SteamVR_Action_Boolean teleportAction;
+	public SteamVR_Action_Boolean grabAction;
+
+	public SteamVR_Action_Boolean GrabPinch;
+
+	public Material[] materials;
+	private MeshRenderer meshrenderer;
+
+	public int arrayIndex = 0;
+
+	void Start () {
+
+		meshrenderer = GetComponent<MeshRenderer> ();
+		//materials = new Material[7];
+		//controller = GetComponent<SteamVR_TrackedController> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//or can use Teleport for touchpad control
+
+		if (SteamVR_Actions._default.myTrigger.GetStateUp(SteamVR_Input_Sources.Any) || Input.GetKeyDown(KeyCode.Space)) {
+
+			if (arrayIndex != materials.Length - 1) {
+
+				arrayIndex++;
+				meshrenderer.material = materials [arrayIndex];
+				Debug.Log ("teleport has been pressed.");
+						
+			} else {
+				arrayIndex = 0;
+				meshrenderer.material = materials [arrayIndex];
+				Debug.Log ("index reset");
+			}
+
+		}
+
+
+
+
+
+	}
+
+
+
+	public bool GetTeleportDown() {
+
+		return teleportAction.GetStateDown(handType);
+	}
+
+
+	public bool GetGrab()
+	{
+		return grabAction.GetState (handType);
+	}
+
+
+}
